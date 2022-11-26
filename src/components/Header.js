970b60ff70befe7cb5c30a77e662e339d5939c0a
mattraydev <mattray.dev@gmail.com>
2022-11-25 17:04:49 -0700

    base setup

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <section className='header-main'>
+            
+            <div>Header</div>
+            <h3>Practice with react components and testing</h3>
+        </section>
+    </>
+  )
+}
+
+export default Header
